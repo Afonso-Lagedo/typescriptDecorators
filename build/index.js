a03065504b0ac53448ca365bd009e3b4ae17ce60
Afonso-cmd <afonso.ur@gmail.com>
2022-08-15 09:23:16 -0300

    In property

--- build/index.js
+++ build/index.js
@@ -2,14 +2,7 @@
 /*
 >> DECORATORS <<
 
-Use in
-Class, Property, methods, Parameters, GETTERS and SETTERS
-
-Use to
-Add resources, monitoring...
-
-Create
-Create the function with default parameters
+in Property
 */
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -17,20 +10,38 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function setIpServer(newIp) {
-    return (target) => {
-        return class extends target {
-            constructor() {
-                super(...arguments);
-                this.ip = newIp;
+function validateName(size) {
+    //target = constructor
+    //key = property
+    return (target, key) => {
+        let test = target[key];
+        const getter = () => test;
+        const setter = (value) => {
+            if (value === "") {
+                console.log("Can't be empty");
+            }
+            else if (value.length < size) {
+                console.log("The minimum size is five");
+            }
+            else {
+                test = value;
             }
         };
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter
+        });
     };
 }
-let Server = class Server {
-};
-Server = __decorate([
-    setIpServer("192.168.15.178")
-], Server);
-const serverOne = new Server();
-console.log(serverOne);
+class Game {
+    constructor(name) {
+        this.name = name;
+    }
+}
+__decorate([
+    validateName(5)
+], Game.prototype, "name", void 0);
+//const game1 = new Game(""); //return: Can't be empty
+//const game1 = new Game("GTA"); //return: The minimum size is five
+const game1 = new Game("FIFA 2022"); //return: FIFA 2022
+console.log(game1.name);
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,46 @@
 /*
 >> DECORATORS <<
 
-Use in 
-Class, Property, methods, Parameters, GETTERS and SETTERS
-
-Use to
-Add resources, monitoring...
-
-Create 
-Create the function with default parameters
+in Property
 */
 
-function setIpServer(newIp: string){
-    return(target:any) =>{//target is constructor of class 
-        return class extends target{
-            ip = newIp;
+function validateName(size:number){
+    //target = constructor
+    //key = property
+    return (target: any, key: string)=>{
+        let test = target[key];
+
+        const getter = () => test;
+
+        const setter = (value: string) => {
+            if(value === ""){
+                console.log("Can't be empty");
+            } else if(value.length < size){
+                console.log("The minimum size is five");
+            } else{
+                test = value;
+            }
         }
+
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter
+        })
     }
 }
 
-@setIpServer("192.168.15.178")
-class Server{
+class Game {
 
+    @validateName(5)
+    name:string;
+
+    constructor(name:string){
+        this.name=name;
+    }
 }
 
-const serverOne = new Server();
+//const game1 = new Game(""); //return: Can't be empty
+//const game1 = new Game("GTA"); //return: The minimum size is five
+const game1 = new Game("FIFA 2022"); //return: FIFA 2022
 
-console.log(serverOne);
+console.log(game1.name);
